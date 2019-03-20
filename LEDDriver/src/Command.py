5e92788251f2d10d75d36e30fb119febf05fef5c
Kopysov Evgeniy <SmileGobo@gmail.com>
2019-03-20 23:22:03 +0300

    Классы команд для управления табло

---
+++ LEDDriver/src/Command.py
@@ -0,0 +1,59 @@
+
+class Base:
+
+
+    Code = 0
+    Length = 0
+    Marker = 0x55
+    ShowFrame  = 0x1
+    ShowColumn = 0x2
+    LoadColumn = 0x3
+    LoadFrame  = 0x4
+    SetBrghtnss= 0x5
+
+
+    def __init__(self, addr = 0, data = []):
+        if addr > 0x70:
+            raise RuntimeError('addr not in range 0 <= addr <= 0x70')
+        
+        self._addr = addr
+        self._data = data
+
+
+    def pack(self):
+        rslt = [self.Marker, secondByte( self.Code, self._addr)] #TODO адрес 5 бит 3 код
+        if 0 != self.Length and 0 != self._addr:
+            rslt.append(self.Length)
+            rslt += self._data
+        return bytes(rslt)
+    
+def secondByte(code, addr):
+    if 0 == addr: 
+        return code
+    return code |(addr << 3)   
+
+def defineCommand(code, len = 0):
+    class Impl(Base):
+        Code = code
+        Length = len
+    return Impl
+
+class LoadFrame(defineCommand(Base.LoadFrame, len = 35)):
+    pass
+
+class LoadColumn(defineCommand(Base.LoadColumn, len = 4)):
+    pass
+
+'''    
+class ShowFrame(Base):
+    Code = Base.ShowFrame
+    def __init__ (self):
+        super().__init__()
+
+class ShowColumn(Base):
+    Code = 
+
+'''
+
+ShowFrame = defineCommand(Base.ShowFrame)
+ShowColumn = defineCommand(Base.ShowColumn)
